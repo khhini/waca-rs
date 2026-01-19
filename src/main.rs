5ce4035b89c1f5b67653b79753152c5eef0223ab
khhini <kiki.h.hutapea@gmail.com>
2026-01-19 23:27:37 +0700

    chores: refactor bookmark using Repository patterns

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,17 @@ enum Command {
 async fn main() -> anyhow::Result<()> {
     let args = Args::parse();
     let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
+    let sqlite_repo = BookmarkSqliteRepo::new(pool);
 
     match args.cmd {
         Some(Command::Add { url, description }) => {
             println!("Adding new bookmarks");
-            let id = add(&pool, url, description).await?;
+            let id = sqlite_repo.add(url, description).await?;
             println!("Added new bookmark id {id}");
         }
         Some(Command::List) => {
-            list(&pool).await?;
+            let recs = sqlite_repo.list().await?;
+            dbg!(recs);
         }
         None => {}
     }
@@ -35,37 +37,51 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn list(pool: &SqlitePool) -> anyhow::Result<()> {
-    let recs = sqlx::query!(
-        r#"
+#[derive(Debug)]
+struct Bookmark {
+    id: i64,
+    url: String,
+    description: String,
+}
+
+struct BookmarkSqliteRepo {
+    pool: SqlitePool,
+}
+
+impl BookmarkSqliteRepo {
+    fn new(pool: SqlitePool) -> Self {
+        BookmarkSqliteRepo { pool }
+    }
+
+    async fn list(self) -> anyhow::Result<Vec<Bookmark>> {
+        let recs = sqlx::query_as!(
+            Bookmark,
+            r#"
 SELECT id,  url,description
 FROM bookmarks
 ORDER BY id
         "#
-    )
-    .fetch_all(pool)
-    .await?;
+        )
+        .fetch_all(&self.pool)
+        .await?;
 
-    for rec in recs {
-        println!("- [{}]({})", &rec.description, &rec.url)
+        Ok(recs)
     }
-    Ok(())
-}
+    async fn add(self, url: String, description: String) -> anyhow::Result<i64> {
+        let mut conn = self.pool.acquire().await?;
 
-async fn add(pool: &SqlitePool, url: String, description: String) -> anyhow::Result<i64> {
-    let mut conn = pool.acquire().await?;
-
-    let id = sqlx::query!(
-        r#"
+        let id = sqlx::query!(
+            r#"
 INSERT INTO bookmarks ( url, description)
 VALUES ( ?1, ?2 )
         "#,
-        url,
-        description
-    )
-    .execute(&mut *conn)
-    .await?
-    .last_insert_rowid();
+            url,
+            description
+        )
+        .execute(&mut *conn)
+        .await?
+        .last_insert_rowid();
 
-    Ok(id)
+        Ok(id)
+    }
 }
